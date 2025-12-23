be68694a9b0f2694c1d1f572b307387a4e7986a1
zZDANTI <Salvadantizz@gmail.com>
2025-12-24 00:27:31 +0100

    v8

--- jellyfin-theme-loader.js
+++ jellyfin-theme-loader.js
@@ -67,9 +67,7 @@
          /*BARRA NAV DE PELICULAS CSS Y JS*/
         loadCSS("https://cdn.jsdelivr.net/gh/MakD/Jellyfin-Media-Bar@latest/slideshowpure.css");
         loadJS("https://cdn.jsdelivr.net/gh/MakD/Jellyfin-Media-Bar@latest/slideshowpure.js");
-
-        /* 💻 DESKTOP Y MOVIL*/
-        loadCSS("https://cdn.jsdelivr.net/gh/lscambo13/ElegantFin@main/Theme/assets/add-ons/media-bar-plugin-support-latest-min.css");
+        
     }
 
     new MutationObserver(mutations => {
