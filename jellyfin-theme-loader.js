982fe1b6711026c9140067cc411e6627beb75faa
zZDANTI <Salvadantizz@gmail.com>
2025-12-28 00:58:37 +0100

    fixed

--- jellyfin-theme-loader.js
+++ jellyfin-theme-loader.js
@@ -67,7 +67,7 @@
          /*BARRA NAV DE PELICULAS CSS Y JS*/
         loadCSS("https://cdn.jsdelivr.net/gh/MakD/Jellyfin-Media-Bar@latest/slideshowpure.css");
         loadJS("https://cdn.jsdelivr.net/gh/MakD/Jellyfin-Media-Bar@latest/slideshowpure.js");
-        loadJS("https://cdn.jsdelivr.net/gh/ZZDANTI/V9@latest/scripts/removeContinue.js");
+        loadJS("https://cdn.jsdelivr.net/gh/ZZDANTI/zZSTREAM@latest/scripts/removeContinue.js");
 
         
     }
